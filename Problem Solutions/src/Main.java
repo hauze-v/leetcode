c55c433718e6e525a192820b94f992075d67f366
hauze-v <hauze_v@protonmail.com>
2020-02-02 12:22:22 -0500

    new validParentheses problem

--- Problem Solutions/src/Main.java
+++ Problem Solutions/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     public static void main(String[] args) {
 
         /*
@@ -10,16 +9,19 @@ public class Main {
         String str = "MCMIV";
         System.out.println("Integer form of Roman Numeral " + str +
                 " is " + ob.romanToDecimal(str));
-         */
 
-        /*
+
         // Longest Common Prefix Problem
         LongestCommonPrefix lcp = new LongestCommonPrefix();
         String[] words = {"Adam", "Evan", "Envelope", "Donkey", "Adult","Adamantine"};
         String [] wordsWithout = {"games", "movies", "bingo", "school", "words", "affiliate"};
         System.out.println("The longest common prefix amongst the list of strings is: " + lcp.longestCommonPrefix(words));
         System.out.println("The longest common prefix amongst the list of strings is: " + lcp.longestCommonPrefix(wordsWithout));
-         */
+        */
 
     }
 }
+
+
+
+
